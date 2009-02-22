0086748ee725fb226769d6560e7fd3e56a02ad31
United20 <United20@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-22 14:15:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/izioseo/tags/1.1.2@96638 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- izioseo.php
+++ izioseo.php
@@ -2750,7 +2750,7 @@ class izioSEO
 	}
 
 	/**
-	 * Setzt alle Links des Strings auf nofollow (RegEx und Callback bei Ronald Kirschler <ronald.kirschler@absofort.de>)
+	 * Setzt alle Links des Strings auf nofollow
 	 *
 	 * @param string $content
 	 * @return string
