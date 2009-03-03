4d5841d9c4ecb2edbaf4af56fe739e104847b82f
United20 <United20@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-03 17:55:18 +0000

    izioSEO v1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/izioseo/tags/1.2.1@99228 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/DB.class.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * Datenbankklasse fuer izioSEO
+ *
+ * @author Mathias Schmidt <united20@united20.de>
+ */
+class DB
+{
+
+	/**
+	 * Datenbankobjekt von Wordpress
+	 */
+	var $db = null;
+
+	/**
+	 * Klassenkonstruktor
+	 */
+	function __construct()
+	{
+		global $wpdb;
+		$this->db = $wpdb;
+		if (! defined('DB_PREFIX'))
+		{
+			define('DB_PREFIX', $this->db->prefix);
+		}
+	}
+
+	/**
+	 * fuehrt einen MySQL-Query aus
+	 *
+	 * @param string $query
+	 * @return ressource
+	 */
+	function query($query)
+	{
+		return $this->db->query($this->replacePrefix($query));
+	}
+
+	/**
+	 * Loeschen eines oder mehrerer Datensaetze aus der Datenbank
+	 *
+	 * @param string $table
+	 * @param string $where
+	 * @return integer
+	 */
+	function delete($table, $where)
+	{
+		return $this->query('DELETE FROM ' . $table . ' WHERE ' . $where);
+	}
+
+	/**
+	 * Leeren einer Datenbanktabelle
+	 *
+	 * @param string $table
+	 * @return integer
+	 */
+	function truncate($table)
+	{
+		return $this->query('TRUNCATE ' . $table);
+	}
+
+	/**
+	 * Update eines Datensatzes
+	 *
+	 * @param string $table
+	 * @param string/array $fields
+	 * @param string $where
+	 * @return integer
+	 */
+	function update($table, $fields, $where)
+	{
+		$query = 'UPDATE ' . $table . ' SET ';
+		if (is_array($fields))
+		{
+			$values = array();
+			foreach ($fields as $field => $value)
+			{
+				$values[] = $field . '="' . addslashes($value) . '"';
+			}
+			$query .= implode(',', $values);
+		}
+		else
+		{
+			$query .= $fields . ' ';
+		}
+		$query .= $where;
+		return $this->query($query);
+	}
+
+	/**
+	 * Fuegt einen Datensatz in eine Datenbanktabelle ein
+	 *
+	 * @param string $table
+	 * @param array $fields
+	 * @param array $options
+	 * @return integer
+	 */
+	function insert($table, $fields, $options = array())
+	{
+		$query = 'INSERT ';
+		if (isset($options['ignore']) && $options['ignore'] == true)
+		{
+			$query .= 'IGNORE ';
+		}
+		$query .= 'INTO ' . $table . ' SET ';
+		$queryFields = array();
+		foreach ($fields as $key => $value)
+		{
+			if (preg_match('/^[0-9]+$/', $key))
+			{
+				$queryFields[] = addslashes($value);
+			}
+			else
+			{
+				$queryFields[] = $key . '="' . addslashes($value) . '"';
+			}
+		}
+		$this->query($query . implode(', ', $queryFields));
+		return mysql_insert_id();
+	}
+
+	/**
+	 * gibt die Resultate eines Queries zurueck
+	 *
+	 * @param string $query
+	 * @return array
+	 */
+	function fetchResults($query)
+	{
+		return $this->db->get_results($this->replacePrefix($query));
+	}
+
+	/**
+	 * gibt einen Wert eines Queries zurueck
+	 *
+	 * @param string $query
+	 * @return mixed
+	 */
+	function fetchOne($query)
+	{
+		return $this->db->get_var($this->replacePrefix($query));
+	}
+	
+	function fetchAssoc($query)
+	{
+		return $this->db->get_row($this->replacePrefix($query), ARRAY_A);
+	}
+
+	/**
+	 * ersetzen des Platzhalters # in einem Query
+	 *
+	 * @param string $query
+	 * @return string
+	 */
+	function replacePrefix($query)
+	{
+		return str_replace('#', DB_PREFIX, $query);
+	}
+
+}
